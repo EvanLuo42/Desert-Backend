72320ae2bc8fc6da241d50d7b636a2d5cace76b8
EvanLuo42 <ziyun.luo@icloud.com>
2022-04-07 09:46:21 +0800

    feat: Request times restriction

---
+++ desert/middleware.py
@@ -0,0 +1,22 @@
+import datetime
+
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+from django.utils.translation import gettext as _
+
+
+class RequestRestrictionMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        last_request = request.session.get('last_request')
+        print(last_request)
+        now = datetime.datetime.now()
+        if last_request is not None:
+            last_request = datetime.datetime.strptime(request.session.get('last_request'), '%Y-%m-%d %H:%M:%S.%f')
+            request.session['last_request'] = str(now)
+            if (now - last_request).seconds < 2:
+                return JsonResponse({'status': 'error', 'message': _('To many request')}, status=400)
+        else:
+            request.session['last_request'] = str(now)
+
+
+
--- desert/settings.py
+++ desert/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'desert.middleware.RequestRestrictionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
