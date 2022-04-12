b11e16d2e88496a93b3c8fa2552fb89283f3e62c
EvanLuo42 <ziyun.luo@icloud.com>
2022-04-12 14:26:21 +0800

    fix: Rate limit not working

--- desert/middleware.py
+++ desert/middleware.py
@@ -10,13 +10,13 @@ from desert import settings
 
 class RequestRestrictionMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        identify = request.session.get('identify')
+        identify = request.META.get('REMOTE_ADDR')
         requested_times = cache.get(identify)
+        print(requested_times)
         if requested_times is not None:
             if int(requested_times) >= settings.REQUEST_LIMIT:
                 return JsonResponse({'status': 'error', 'message': _('Too many requests')}, status=400)
             else:
                 cache.set(identify, requested_times + 1)
         else:
-            identify = request.session['identify'] = uuid.uuid4()
             cache.set(identify, 1, settings.REQUEST_LIMIT_TIME)
