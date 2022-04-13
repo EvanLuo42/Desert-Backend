6a7899c688b9d230a79fbeddfcd9d612e8f8057f
EvanLuo42 <ziyun.luo@icloud.com>
2022-04-13 10:51:25 +0800

    fix: Could not visit Admin Site because of UA

--- desert/middleware.py
+++ desert/middleware.py
@@ -28,11 +28,12 @@ class ServerSafeGuardMiddleware(MiddlewareMixin):
 class UserAgentMiddleware(MiddlewareMixin):
     def process_request(self, request):
         user_agent = request.headers.get('User-Agent').split(' ')
-        if len(user_agent) != 2 or len(user_agent[1].split('/')) != 2:
+        if request.path.find('/desert/admin/') != -1 or request.path.find('/static/') != -1:
+            pass
+        elif len(user_agent) != 2 or len(user_agent[1].split('/')) != 2:
             return JsonResponse({'status': 'error', 'message': _('Argument missing')}, status=400)
         elif user_agent[0] != 'Desert/' + settings.API_VERSION:
             return JsonResponse({'status': 'error', 'message': _('API version does not support')}, status=400)
         elif user_agent[1].split('/')[0] != 'Android' and user_agent[1].split('/')[0] != 'iOS' \
                 and user_agent[1].split('/')[0] != 'iPadOS':
             return JsonResponse({'status': 'error', 'message': _('Platform does not support')}, status=400)
-
