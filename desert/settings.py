19667eba0a16ab9198fa26ade9651a09bdc2946e
EvanLuo42 <ziyun.luo@icloud.com>
2022-04-15 13:05:31 +0800

    fix: Redis connect failed

--- desert/settings.py
+++ desert/settings.py
@@ -89,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379',
+        'LOCATION': 'redis://127.0.0.1:6379',
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
